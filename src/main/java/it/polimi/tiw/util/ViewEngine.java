7b498a374aed843cdeb2a8c5b1b9148869bf459d
Filippo Raimondi <92033111+filo3r@users.noreply.github.com>
2024-10-31 11:56:29 +0100

    Version changes in pom

--- src/main/java/it/polimi/tiw/util/ViewEngine.java
+++ src/main/java/it/polimi/tiw/util/ViewEngine.java
@@ -1,6 +1,6 @@
 package it.polimi.tiw.util;
 
-import jakarta.servlet.ServletContext;
+import javax.servlet.ServletContext;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
